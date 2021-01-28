4233d8712c509344aef3b6034ecf7ffdf3d1acb8
Jacob John Nilsson <jacobjnilsson@gmail.com>
2021-01-28 13:03:04 +0100

    more data print

--- data_parser.py
+++ data_parser.py
@@ -10,31 +10,44 @@ def parse(datafile="swefn.xml"):
     frames = 0
     framesWithExample = 0
     totalExamples = 0
+
     for lexicalEntry in lexicalEntries:
         frames += 1
-        j = 0
+        examples = 0
+        lus = 0
+        coreFE = 0
         sense = lexicalEntry.find("Sense")
-        children = []
+        # children = []
         for child in sense:
             if (
                 child.tag
                 == "{http://spraakbanken.gu.se/eng/research/infrastructure/karp/karp}example"
             ):
-                children.append(child)
-                j += 1
-        if j > 0:
-            framesWithExample += 1
-            totalExamples += j
-            print(lexicalEntry.tag, lexicalEntry.attrib)
-            # for child in children:
-            # print(
-            #     child.tag, child.attrib
-            # )  # Make better print or don't print at all
-            print(j)
+                # children.append(child)
+                examples += 1
+            elif child.attrib["att"] == "LU":
+                lus += 1
+            elif child.attrib["att"] == "coreElement":
+                coreFE += 1
 
-    print("Number of frames: " + str(frames))
-    print("Of which had examples: " + str(framesWithExample))
+        if examples > 0:
+            framesWithExample += 1
+            totalExamples += examples
+        print(sense.attrib["id"])
+        # for child in children:
+        # print(
+        #     child.tag, child.attrib
+        # )  # Make better print or don't print at all
+        print("Examples:    " + str(examples))
+        print("LUs:         " + str(lus))
+        print("Core FEs:    " + str(coreFE))
+        print("")
+
+    print("Number of frames:        " + str(frames))
+    print("Of which had examples:   " + str(framesWithExample))
+    print("Examples:                " + str(totalExamples))
     print(
-        "Avg. example per frame with example: " + str(totalExamples / framesWithExample)
+        "Avg. number of examples per frame with examples: "
+        + str(totalExamples / framesWithExample)
     )
     return root
